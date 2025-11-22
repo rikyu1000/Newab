4de7615195105e6130f38aa6b00a26c9edb3ec00
rikyu1000 <nakaken5428@gmail.com>
2025-11-22 22:12:47 +0900

    fix: remove static export config for vercel

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
-// GitHub repository name
-const repoName = "Newab";
-
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  // Configure basePath / assetPrefix for GitHub Pages project site:
-  // https://<username>.github.io/Newtab/
-  basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
