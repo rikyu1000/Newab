da0de254ba495d6fd654e1f6ec0a25b42e3eee2a
rikyu1000 <nakaken5428@gmail.com>
2025-11-23 19:49:58 +0900

    fix: ensure https protocol in redirect uri

--- src/lib/google.ts
+++ src/lib/google.ts
@@ -1,11 +1,14 @@
 import { google } from "googleapis";
 
 export const getGoogleAuthClient = () => {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  const urlWithProtocol = baseUrl.startsWith("http")
+    ? baseUrl
+    : `https://${baseUrl}`;
+
   return new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    `${
-      process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
-    }/api/auth/callback`
+    `${urlWithProtocol}/api/auth/callback`
   );
 };
